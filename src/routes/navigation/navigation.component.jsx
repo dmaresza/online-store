165da693e5ecdc0e618b836191d826ea4fa3a0ba
dmaresza <daniel.a.mares.z@gmail.com>
2025-05-01 12:34:09 -0400

    Fixed some styling issues with css class names

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -23,17 +23,15 @@ const Navigation = () => {
           <Link className='nav-link' to='/shop'>
             SHOP
           </Link>
-          {
-            currentUser ? (
-              <span className='nav-link' onClick={signOutUser}>
-                SIGN OUT
-              </span>
-            ) : (
-              <Link className='nav-link' to='/auth'>
-                SIGN IN
-              </Link>
-            )
-          }
+          {currentUser ? (
+            <span className='nav-link' onClick={signOutUser}>
+              SIGN OUT
+            </span>
+          ) : (
+            <Link className='nav-link' to='/auth'>
+              SIGN IN
+            </Link>
+          )}
           <CartIcon />
         </div>
         {isCartOpen && <CartDropdown />}
