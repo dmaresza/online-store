68634d046784e3aeff5481d1f983dcd2b905067a
dmaresza <daniel.a.mares.z@gmail.com>
2025-05-06 10:36:10 -0400

    Added routes to individual apparel categories from home page

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,8 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 import './directory-item.styles.scss';
 
-const DirectoryItem = ({ imageUrl, title }) => {
+const DirectoryItem = ({ category }) => {
+  const { title, imageUrl, route } = category;
+
+  const navigate = useNavigate();
+  const onNavigateHandler = () => navigate(route);
+
   return (
-    <div className='directory-item-container'>
+    <div className='directory-item-container' onClick={onNavigateHandler}>
       <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className='body'>
         <h2>{title}</h2>
