a5f3c7ec614ab6c022b8b70d4ed0e669ad3cc2bc
dmaresza <daniel.a.mares.z@gmail.com>
2025-05-15 15:35:08 -0400

    added the redux-persist library to store cart information in local storage

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,23 @@
 import { compose, createStore, applyMiddleware } from "redux";
-// import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
 import { rootReducer } from "./root-reducer";
+import logger from "redux-logger";
+// import { loggerMiddleware } from "./middleware/logger";
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-  console.log('type: ', action.type);
-  console.log('payload: ', action.payload);
-  console.log('currentState: ', store.getState());
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user']
+};
 
-  next(action);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-  console.log('next state: ', store.getState());
-}
-
-// const middleWares = [logger];
-const middleWares = [loggerMiddleware];
+const middleWares = [process.env.NODE_ENV != 'production' && logger].filter(Boolean);
+// const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
